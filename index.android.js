d499c66df99863081765dd00580bc0a0f37be436
qiaolele <2046882662@qq.com>
2017-09-21 16:19:24 +0800

    Initial commit basehtml

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, Text, Image } from 'react-native';
+
+
+// 注意，这里用引号括起来的'AwesomeProject1'必须和你init创建的项目名一致
+AppRegistry.registerComponent('AwesomeProject1', () => AwesomeProject1);
